3279298091eb73283713b883e8b100a38dea09aa
Vimal pandey <160587834+vimalpandey1111@users.noreply.github.com>
2025-06-22 08:09:40 +0000

    today work

---
+++ 01_basics/01_varibals.js
@@ -0,0 +1,9 @@
+const accountId = 122121
+let accountemail ="vimal2google.com"
+var accountpassword = "1234abcd"
+accountCity = "khalilabad"
+
+
+console.log(accountId);
+
+console.table([accountId,accountemail,accountpassword,accountCity]);
