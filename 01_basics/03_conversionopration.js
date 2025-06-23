d6f640b230880fd73a8809edb4a2a2b0fb4aa4f5
Vimal pandey <160587834+vimalpandey1111@users.noreply.github.com>
2025-06-23 09:31:45 +0000

    Conversion Know

---
+++ 01_basics/03_conversionopration.js
@@ -0,0 +1,17 @@
+let score= "33"
+  
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
